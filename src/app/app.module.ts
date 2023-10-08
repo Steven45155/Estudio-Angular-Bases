cca05c3009d993051ff63242c790f2d4d6c7bb07
Steven_Suarez <stiven451556@gmail.com>
2023-10-08 18:37:26 -0500

    fin de sesion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/components/counter.module';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/Heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
